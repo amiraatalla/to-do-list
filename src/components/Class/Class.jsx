c9ae5cfe8925e5da5639abdb673c302963b36b0f
amiraatalla <amiraatalla63@gmail.com>
2023-12-26 15:11:34 +0200

    :sparkles: diffrent between class component and function component

---
+++ src/components/Class/Class.jsx
@@ -0,0 +1,11 @@
+
+import React, { Component } from 'react';
+
+export default class Class extends Component{
+
+    render(){
+        return<>
+        <h1>Class Component</h1>
+        </>
+    }
+}
---
+++ src/components/Func/Func.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+export default function Func() {
+
+    return <>
+        <h1>Function Component</h1>
+        <hr />
+    </>
+
+
+}
