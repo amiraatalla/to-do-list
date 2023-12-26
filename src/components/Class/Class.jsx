c10b46e38270846eae57ac624a012d95c2b33c3b
amiraatalla <amiraatalla63@gmail.com>
2023-12-26 15:30:42 +0200

    :sparkles: hooks

--- src/components/Class/Class.jsx
+++ src/components/Class/Class.jsx
@@ -1,11 +1,16 @@
 
 import React, { Component } from 'react';
 
-export default class Class extends Component{
-
-    render(){
-        return<>
-        <h1>Class Component</h1>
+export default class Class extends Component {
+    state = { myName: "Amira Atallah" };
+    changeName = () => {
+        this.setState({ myName: "Amira Reda" });
+    }
+    render() {
+        return <>
+            <h1>Class Component</h1>
+            <h3>{this.state.myName}</h3>
+            <button className='btn-danger' onClick={this.changeName}>change name</button>
         </>
     }
 }
--- src/components/Func/Func.jsx
+++ src/components/Func/Func.jsx
@@ -1,12 +1,26 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Func() {
+    let [myName, updatedName] = useState("Amira Atallah");
+    let [count, setCount] = useState(0);
+    function changeName() {
+        updatedName("Amira Reda");
+    }
+    function changeCount() {
+        setCount(Math.random() * 10);
 
+    }
     return <>
         <h1>Function Component</h1>
+        <h3>{myName}</h3>
+        <button onClick={changeName}>changeName</button>
+        <h3>{count}</h3>
+        <button onClick={changeCount}>changeCount
+        </button>
         <hr />
     </>
 
+    //view->Hooks useState useEffect
 
 }
